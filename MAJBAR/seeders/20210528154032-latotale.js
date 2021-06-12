30742b38f0e02bba553554a66c8a95319e782124
Kurolius <ymajbar.pro@gmail.com>
2021-06-12 19:51:03 +0100

    Blog1.9

--- MAJBAR/seeders/20210528154032-latotale.js
+++ MAJBAR/seeders/20210528154032-latotale.js
@@ -1,60 +1,92 @@
 'use strict';
-const faker = require('faker')
-var _ = require('underscore');
-const users = [...Array(20)].map((user) => (
-  {
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(8),
-    role : faker.helpers.randomize(['admin', 'author', 'guest']),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-))
-
-const tags = [...Array(10)].map((tags) => (
-  {
-    name: faker.lorem.word(),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-))
-const Article = [...Array(200)].map((Article) => (
-  {
-    title: faker.name.title(),
-    content:faker.lorem.paragraph(),
-    published:1,
-    UserId:faker.helpers.randomize(_.range(1,20,1)),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-))
-
-const Comment = [...Array(2000)].map((Comment) => (
-  {
-    content:faker.lorem.paragraph(),
-    ArticleId:faker.helpers.randomize(_.range(401,600,1)),
-    createdAt: new Date(),
-    updatedAt: new Date()
-  }
-))
-
+const faker = require('faker');
+const { Article, sequelize } = require('../models');
+const { User } = require('../models');
+const { Tag } = require('../models');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    //return queryInterface.bulkInsert('Tags', tags, {})
-    //return queryInterface.bulkInsert('Users', users, {})
-    //return queryInterface.bulkInsert('Articles', Article, {})
-    //return queryInterface.bulkInsert('Comments', Comment, {});
-    for(var i=401;i<=600;i++){
-    for(var j=1;j<=10;j++){
-      await queryInterface.bulkInsert('ArticleTags', [{
-        ArticleId:  i,
-        Tagid:  j ,
-        createdAt: new Date(),
+
+    await queryInterface.bulkInsert('Users',
+     [...Array(20)].map((user) => (
+      {
+        email: faker.internet.email(),
+        username: faker.internet.userName(),
+        password: faker.internet.password(8),
+        role: faker.helpers.randomize(['guest', 'admin', 'author']),
+        createdAt: faker.date.between('2000-01-01', '2021-12-31'),
         updatedAt: new Date()
-      }], {});
-    }
-    }
+      }
+    )) , {});
+    console.log('Users DONE');
+
+    const mfs = await User.findAll({
+      attributes: ['id']
+    });
+
+    await queryInterface.bulkInsert('Articles',
+      [...Array(200)].map((Article) => (
+        {
+          title: faker.name.title(),
+          content: faker.lorem.paragraphs(2),
+          published: 1,
+          UserId: faker.random.arrayElement(JSON.parse(JSON.stringify(mfs))).id,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ))
+      , {});
+    console.log('Articles DONE');
+
+    await queryInterface.bulkInsert('Tags',
+      [...Array(10)].map((Tag) => (
+        {
+          name: faker.lorem.sentence(3, 3),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ))
+      , {});
+    console.log('Tags DONE');
+
+    const mfs_articles = await Article.findAll({
+      attributes: ['id']
+    });
+
+    //console.log(faker.random.arrayElement(JSON.parse(JSON.stringify(mfs_articles))).id);
+
+    await queryInterface.bulkInsert('Comments',
+      [...Array(2000)].map((Comment) => (
+        {
+          content: faker.lorem.lines(faker.datatype.number({ 'min': 1, 'max': 4 })),
+          ArticleId: faker.random.arrayElement(JSON.parse(JSON.stringify(mfs_articles))).id,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ))
+      , {});
+    console.log('Comments DONE');
+
+    const mfs_tags = await Tag.findAll({
+      attributes: ['id']
+    });
+
+
+    var art_last = JSON.parse(JSON.stringify(mfs_articles)).length - 1
+    var tag_last = JSON.parse(JSON.stringify(mfs_tags)).length - 1
+
+    console.log('Adding ArticleTags... (This will take a while)');
+    for (var i = 0; i <= art_last; i++) {
+      for (var j = 0; j <= tag_last; j++) {
+        await queryInterface.bulkInsert('ArticleTags', [{
+          ArticleId: JSON.parse(JSON.stringify(mfs_articles))[i].id,
+          Tagid: JSON.parse(JSON.stringify(mfs_tags))[j].id,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }], {})
+      }
+    };
+    console.log('ArticleTags DONE');
+
   },
 
   down: async (queryInterface, Sequelize) => {
